4adaa08f88aced66ad3e9cc1ba5edb7e118903a6
Rosemary Barrass <rosiebarrass@gmail.com>
2024-05-02 12:24:41 -0400

    add SMOTE to random forest

--- model/src/random_forest.py
+++ model/src/random_forest.py
@@ -5,6 +5,13 @@ from sklearn.model_selection import KFold
 from sklearn.metrics import accuracy_score, precision_score, recall_score
 import matplotlib.pyplot as plt
 import seaborn as sns
+from statistics import mean
+from imblearn.over_sampling import SMOTE
+
+def apply_SMOTE(X_train, y_train):
+    smote = SMOTE(sampling_strategy='not majority')
+    X_balanced, y_balanced = smote.fit_resample(X_train, y_train)
+    return X_balanced, y_balanced
 
 # Get the current working directory
 current_dir = os.getcwd()
@@ -27,11 +34,13 @@ for type in data_types:
     y = data['ordinal__Status']
     # Split the data into training and testing sets
     kf = KFold()
+    best_per_fold = []
     for i, (train_index, test_index) in enumerate(kf.split(X)):
         X_train = X.iloc[train_index]
         X_test = X.iloc[test_index]
         y_train = y.iloc[train_index]
         y_test = y.iloc[test_index]
+        X_balanced, y_balanced = apply_SMOTE(X_train, y_train)
         # Define different estimators (number of trees)
         estimators = list(range(10, 150, 5))
         # Initialize lists to store metrics
@@ -42,17 +51,17 @@ for type in data_types:
         for est in estimators:
             # Train RFC model
             rfc = RandomForestClassifier(n_estimators=est)
-            rfc.fit(X_train, y_train)
+            rfc.fit(X_balanced, y_balanced)
             # Predict on the test set
             y_pred = rfc.predict(X_test)
             # Compute accuracy score
             accuracy = accuracy_score(y_test, y_pred)
             accuracy_scores.append(accuracy)
             # Compute the precision score
-            precision = precision_score(y_test, y_pred, average='macro')
+            precision = precision_score(y_test, y_pred, average='weighted')
             precision_scores.append(precision)
             # Compute the recall score
-            recall = recall_score(y_test, y_pred, average='macro')
+            recall = recall_score(y_test, y_pred, average='weighted')
             recall_scores.append(recall)
         # Plot the accuracy scores for different estimators
         plt.figure(figsize=(10, 6))
@@ -61,7 +70,7 @@ for type in data_types:
         plt.ylabel('Accuracy Score')
         plt.title(f'Random Forest Accuracy vs. Number of Trees ({type})')
         plt.grid(True)
-        plt.savefig(os.path.join(viz_dir,'accuracy', f'Kfold{i}_{type}_accuracy_random_forest_num_trees.png'))
+        plt.savefig(os.path.join(viz_dir,'accuracy', f'SMOTE_Kfold{i}_{type}_accuracy_random_forest_num_trees.png'))
         plt.close()
         # Plot the precision scores for different estimators
         plt.figure(figsize=(10, 6))
@@ -70,7 +79,7 @@ for type in data_types:
         plt.ylabel('Precision Score')
         plt.title(f'Random Forest Precision vs. Number of Trees ({type})')
         plt.grid(True)
-        plt.savefig(os.path.join(viz_dir,'precision', f'Kfold{i}_{type}_precision_random_forest_num_trees.png'))
+        plt.savefig(os.path.join(viz_dir,'precision', f'SMOTE_Kfold{i}_{type}_precision_random_forest_num_trees.png'))
         plt.close()
         # Plot the recall scores for different estimators
         plt.figure(figsize=(10, 6))
@@ -79,15 +88,16 @@ for type in data_types:
         plt.ylabel('Recall Score')
         plt.title(f'Random Forest Recall vs. Number of Trees ({type})')
         plt.grid(True)
-        plt.savefig(os.path.join(viz_dir,'recall', f'Kfold{i}_{type}_recall_random_forest_num_trees.png'))
+        plt.savefig(os.path.join(viz_dir,'recall', f'SMOTE_Kfold{i}_{type}_recall_random_forest_num_trees.png'))
         plt.close()
         # Find the highest accuracy score
         highest_accuracy = max(accuracy_scores)
+        best_per_fold.append(highest_accuracy)
         # Find the index of the highest accuracy score
         index_of_highest_accuracy = accuracy_scores.index(highest_accuracy)
         # Train RFC model
         rfc = RandomForestClassifier(n_estimators=estimators[index_of_highest_accuracy])
-        rfc.fit(X_train, y_train)
+        rfc.fit(X_balanced, y_balanced)
         # Predict on the test set
         y_pred = rfc.predict(X_test)
         # Retrieve feature importance values
@@ -108,8 +118,10 @@ for type in data_types:
         plt.xlabel('Feature Importance')
         plt.ylabel('Feature')
         plt.title(f'Random Forest Classifier Feature Importance ({type})')
-        plt.savefig(os.path.join(viz_dir,'weights_and_importance', f'Kfold{i}_{type}_rfc_feature_importance.png'))
+        plt.savefig(os.path.join(viz_dir,'weights_and_importance', f'SMOTE_Kfold{i}_{type}_rfc_feature_importance.png'))
         plt.close()
         # Save the feature importance data as a CSV file
-        importance_rfc_df.to_csv(os.path.join(viz_dir, 'weights_and_importance', f'Kfold{i}_{type}_rfc_feature_importance.csv'), index=False)
+        importance_rfc_df.to_csv(os.path.join(viz_dir, 'weights_and_importance', f'SMOTE_Kfold{i}_{type}_rfc_feature_importance.csv'), index=False)
         print(f"Random Forest Classifier feature importance data saved to {os.path.join(viz_dir, 'weights_and_importance', f'Kfold{i}_{type}_rfc_feature_importance.csv')}")
+    mean_accuracy = mean(best_per_fold)
+    print(f'Best accuracies per fold: {mean_accuracy}')
